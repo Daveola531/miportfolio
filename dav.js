6e72f7589a09953f6b78f3f38e6bdf2dfe276b6f
Daveola531 <roserose67891@gmail.com>
2026-07-14 11:55:28 +0000

    Implement dual counter functionality in dav.js
    Add two counters that increment over time.

--- dav.js
+++ dav.js
@@ -11,3 +11,31 @@ var observer = new IntersectionObserver(function (entries) {
 for (var j = 0; j < animatedEls.length; j++) {
   observer.observe(animatedEls[j]);
 }
+
+
+let num=0
+let target=10 
+let counter=document.getElementById('count');
+ let interval=setInterval(Dav,1000);
+
+ function Dav(){
+  num++;
+  counter.innerHTML=num + "+";
+  if(num>=target){
+clearInterval(interval)
+  }
+ }
+
+
+ let numm=0
+let targett=10 
+let counterr=document.getElementById('countt');
+ let intervals=setInterval(Davv,1000);
+
+ function Davv(){
+  numm++;
+  counterr.innerHTML=numm + "y" + "+";
+  if(numm>=2){
+clearInterval(intervals)
+  }
+ }
